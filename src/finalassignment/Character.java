87f86ffd6af89b30f4050972617c6d1fbd5f62d2
343120135 <343120135@C-1B0VHB2.tdsb.on.ca>
2026-06-04 10:17:26 -0400

    Merge origin/master

--- src/finalassignment/Character.java
+++ src/finalassignment/Character.java
@@ -2,38 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package javaapplication8;
+package finalassignment;
 
 /**
  *
  * @author 343120135
  */
  import processing.core.PApplet;
+ import processing.core.PImage;
 
 public class Character extends PApplet {
     public int x;
     public int y;
-    private int speed;
+    private PImage image;
     private PApplet app;
     
-    public Character(PApplet p, int x, int y, int speed){
+    public Character(PApplet p, int x, int y, String imagePath){
 	this.app = p;
         this.x = x;
         this.y = y;
-        this.speed = speed;
+        this.image = app.loadImage(imagePath);
     }
     
     public void move(int dx, int dy){
-        x += (dx + speed);
+        x += dx;
         y+= dy;
     }
     
     public void draw(){
-        app.fill(255, 0, 0); 
-        app.rect(x, y, 60, 25);
-        
-        app.fill(0);
-        app.ellipse(x + 10, y + 25, 15, 15);
-        app.ellipse(x + 50, y + 25, 15, 15); 
+        app.image(image, x, y);
     }
 }
--- src/finalassignment/Menu.java
+++ src/finalassignment/Menu.java
@@ -1,5 +1,6 @@
-package javaapplication8;
+package finalassignment;
 import processing.core.PApplet;
+import processing.core.PImage;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -11,27 +12,29 @@ import processing.core.PApplet;
  */
 public class Menu extends PApplet {
     Character ÑustaHuillac;
+    private PImage bg;
     int s;
     String userInput = "";
-   private Button startbutton, helpbutton; 
+   private Button startbutton; 
     int stage =0;
        
     public void settings() {
         size(600,600);
     }
     public void setup(){    
-        background(100,100,100);
-        textSize(20);
-        
+        bg = loadImage("images/background.jpg");
+        textSize(66);
+        ÑustaHuillac = new Character(this, 0, 200, "images/warrior1.png");
+        startbutton = new Button(this, 100, 200, "images/start-button.png");
     }
     
     public void draw(){
-        background(132,37,147);
+        image(bg, 0, 0, width, height);
         
         if (stage == 0) {
             fill(0);
-            text("Story of La Tirana:", 20, 50);
-            text(userInput, 20, 100);
+            text("Story of La Tirana:", 25, 50);
+            startbutton.draw();
         } else if (stage == 1) {
             ÑustaHuillac.draw();
             
@@ -52,16 +55,11 @@ public class Menu extends PApplet {
         
         
     }
-    public void keyPressed() {
-        if (stage == 0) {
-            if (keyCode == ENTER) {
-                stage = 1;
-                s = Integer.parseInt(userInput);
-                ÑustaHuillac = new Character(this, 0, 200, s);
-            } else if (key != CODED) {
-                userInput += key;
-            }
+    public void mousePressed() {
+         if (stage == 0 ) {
+        if (startbutton.isClicked(mouseX, mouseY)) {
+        stage = 1;
         }
     }
+  }
 }
-
